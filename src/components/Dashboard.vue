3cce1cf9455f0341320c2f730d9ca5dfda3a8a0a
Abduqodir0205 <abduqodirdeveloper@gmail.com>
2024-02-07 21:23:38 +0500

    dashboard-start

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,78 @@
 <template>
     <div class="dashboard">
-       <div class="dashboard-left"></div>
+       <div class="dashboard-left">
+        <div class="dashboard-left-content">
+            <div class="dashboard-left-content-nextAppointment"></div>
+            <div class="dashboard-left-content-recentDeals">
+                <div>
+                    <h2>Recent Deals</h2>
+                    <router-link class="recentDealsShowAll" to="/deals">
+                        <p>View All</p>
+                    </router-link>
+                </div>
+                <div class="dashboard-left-content-recentDeals-deal">
+                    <div class="dashboard-left-content-recentDeals-deal-avatar">
+                        <img src="../assets/images/png/Avatar.png" alt="avatar">
+                    </div>
+                    <div class="dashboard-left-content-recentDeals-deal-date">
+                        <span>
+                            <h3>319 Haul Road</h3>
+                            <h4>$5750</h4>
+                        </span>
+                        <span>
+                            <p>Glenrock,WY</p>
+                            <p>Nov 14, 07:00 AM</p>
+                        </span>
+                    </div>
+                </div>
+                <div class="dashboard-left-content-recentDeals-deal">
+                    <div class="dashboard-left-content-recentDeals-deal-avatar">
+                        <img src="../assets/images/png/Avatar.png" alt="avatar">
+                    </div>
+                    <div class="dashboard-left-content-recentDeals-deal-date">
+                        <span>
+                            <h3>47 Spruce Drive</h3>
+                            <h4>$5750</h4>
+                        </span>
+                        <span>
+                            <p>Glenrock,WY</p>
+                            <p>Nov 14, 07:00 AM</p>
+                        </span>
+                    </div>
+                </div>
+                <div class="dashboard-left-content-recentDeals-deal">
+                    <div class="dashboard-left-content-recentDeals-deal-avatar">
+                        <img src="../assets/images/png/Avatar.png" alt="avatar">
+                    </div>
+                    <div class="dashboard-left-content-recentDeals-deal-date">
+                        <span>
+                            <h3>165 Belmont Drive</h3>
+                            <h4>$5750</h4>
+                        </span>
+                        <span>
+                            <p>Glenrock,WY</p>
+                            <p>Nov 14, 07:00 AM</p>
+                        </span>
+                    </div>
+                </div>
+                <div class="dashboard-left-content-recentDeals-deal">
+                    <div class="dashboard-left-content-recentDeals-deal-avatar">
+                        <img src="../assets/images/png/Avatar.png" alt="avatar">
+                    </div>
+                    <div class="dashboard-left-content-recentDeals-deal-date">
+                        <span>
+                            <h3>1538 Hammer Road</h3>
+                            <h4>$5750</h4>
+                        </span>
+                        <span>
+                            <p>Glenrock,WY</p>
+                            <p>Nov 14, 07:00 AM</p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+       </div>
        <div class="dashboard-right"></div>
     </div>
 </template>

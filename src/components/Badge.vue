9737cdba3a5fa1366961554433596cb2911c97ef
Abduqodir0205 <abduqodirdeveloper@gmail.com>
2024-02-12 10:13:07 +0500

    dashboard_comlited

---
+++ src/components/Badge.vue
@@ -0,0 +1,23 @@
+<template>
+  <div><p>IN PROGRESS</p></div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+div {
+  border-radius: 100px;
+  padding: 10px 20px;
+  width: 120px;
+  height: 40px;
+  background: var(--primary-blue-light);
+}
+p {
+  font-family: var(--font-family);
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 167%;
+  text-transform: uppercase;
+  color: var(--primary-blue);
+}
+</style>
---
+++ src/components/Customer.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="dashboard-right-customers-user">
+    <div class="dashboard-right-customers-user-avatar">
+      <img src="../assets/images/png/Avatar.png" alt="avatar" />
+    </div>
+    <div class="dashboard-right-customers-user-date">
+      <div>
+        <h2>Deanna Annis</h2>
+        <p>deannannis@gmail.com</p>
+      </div>
+      <svg
+        width="18"
+        height="18"
+        viewBox="0 0 18 18"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M3.61667 15.2667C3.10833 15.2667 2.63333 15.0917 2.29167 14.7667C1.85833 14.3583 1.65 13.7417 1.725 13.075L2.03333 10.375C2.09167 9.86667 2.4 9.19167 2.75833 8.82501L9.6 1.58334C11.3083 -0.224995 13.0917 -0.274995 14.9 1.43334C16.7083 3.14167 16.7583 4.92501 15.05 6.73334L8.20833 13.975C7.85833 14.35 7.20833 14.7 6.7 14.7833L4.01667 15.2417C3.875 15.25 3.75 15.2667 3.61667 15.2667ZM12.275 1.42501C11.6333 1.42501 11.075 1.82501 10.5083 2.42501L3.66667 9.67501C3.5 9.85001 3.30833 10.2667 3.275 10.5083L2.96667 13.2083C2.93333 13.4833 3 13.7083 3.15 13.85C3.3 13.9917 3.525 14.0417 3.8 14L6.48333 13.5417C6.725 13.5 7.125 13.2833 7.29167 13.1083L14.1333 5.86667C15.1667 4.76667 15.5417 3.75001 14.0333 2.33334C13.3667 1.69167 12.7917 1.42501 12.275 1.42501Z"
+          fill="#7E92A2"
+        />
+        <path
+          d="M13.45 8.12501C13.4333 8.12501 13.4083 8.12501 13.3917 8.12501C10.7917 7.86667 8.7 5.89167 8.3 3.30834C8.25 2.96667 8.48333 2.65001 8.825 2.59167C9.16667 2.54167 9.48333 2.77501 9.54167 3.11667C9.85833 5.13334 11.4917 6.68334 13.525 6.88334C13.8667 6.91667 14.1167 7.22501 14.0833 7.56667C14.0417 7.88334 13.7667 8.12501 13.45 8.12501Z"
+          fill="#7E92A2"
+        />
+        <path
+          d="M16.5 17.9583H1.5C1.15833 17.9583 0.875 17.675 0.875 17.3333C0.875 16.9917 1.15833 16.7083 1.5 16.7083H16.5C16.8417 16.7083 17.125 16.9917 17.125 17.3333C17.125 17.675 16.8417 17.9583 16.5 17.9583Z"
+          fill="#7E92A2"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+.dashboard-right-customers-user {
+  margin-top: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  &-avatar {
+    border-radius: 25px;
+    width: 44px;
+    height: 44px;
+    img {
+      width: inherit;
+      height: inherit;
+      border-radius: inherit;
+    }
+  }
+  &-date {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    p {
+      font-family: var(--font-family);
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 193%;
+      color: var(--grey-grey-70);
+    }
+  }
+}
+</style>
